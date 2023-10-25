907e3200403592f8195f66ceda5851ad6a45695f
CaelaMills <caela.cm04@gmail.com>
2023-10-25 12:25:28 -0400

    Commit

--- FlipperGame.java
+++ FlipperGame.java
@@ -1,400 +1,161 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.Timer;
 
-
-
-
-public class FlipperGame extends JFrame implements ActionListener
-{
-
-
-
-
-    private static final String[] CARD_NAMES = {"cow.png", "cutebird.png", "fox.png", "meow.png",
-            "OVO.png", "squirrel.png", "teddybear.png", "woof.png"};
-
-
-
-
-    private int numMoves;
+public class FlipperGame extends JFrame implements ActionListener {
+    private JPanel panel;
+    private JLabel titleLabel;
+    private JButton[] buttons;
+    private ImageIcon[] icons;
+    private int[] cardNumbers;
+    private int[] selectedCards;
+    private int[] matchedCards;
+    private int numCards;
     private int numMatches;
+    private int numAttempts;
+    private Timer timer;
+    private JLabel timerLabel;
+    private int timeLeft;
 
+    public FlipperGame(int numCards) {
+        this.numCards = numCards;
+        this.numMatches = 0;
+        this.numAttempts = 0;
+        this.timeLeft = 300; //300 seconds = 5 minutes
+        this.icons = new ImageIcon[numCards];
+        this.cardNumbers = new int[numCards];
+        this.selectedCards = new int[2];
+        this.matchedCards = new int[numCards];
+        this.timer = new Timer(1000, this);
 
+        // Create panel and buttons
+        this.panel = new JPanel(new GridLayout(0, 4));
+        this.buttons = new JButton[numCards];
+        for (int i = 0; i < numCards; i++) {
+            this.buttons[i] = new JButton();
+            this.buttons[i].addActionListener(this);
+            this.panel.add(this.buttons[i]);
+        }
 
+        // Set up timer
+        this.timerLabel = new JLabel("Time left: " + this.timeLeft + " seconds");
+        this.timerLabel.setHorizontalAlignment(JLabel.CENTER);
+        this.timerLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        this.add(this.timerLabel, BorderLayout.NORTH);
 
-    private Card[] cards;
-    private JPanel cardsPanel;
-    private JLabel movesLabel;
-    private JLabel matchesLabel;
-
-
-
-
-    private Card firstOpenedCard;
-    private Card secondOpenedCard;
-
-
-
-
-    private JButton continueButton;
-    private JButton exitButton;
-
-
-
-
-    public FlipperGame()
-    //Initialize the board, the cards, and the labels
-    {
-        setTitle("Flipper Memory Game");
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        setLayout(new BorderLayout());
-        setSize(500, 500);
+        // Set up frame
+        setTitle("Flipper Card Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        this.add(this.panel, BorderLayout.CENTER);
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        titleLabel = new JLabel("Welcome to the Flipper (Memory Card) Game!");
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(titleLabel);
 
-
-
-
-
-
-
-
-        movesLabel = new JLabel();
-        matchesLabel = new JLabel();
-
-
-
-
-        JPanel gameStatsPanel = new JPanel(new FlowLayout());
-        gameStatsPanel.add(movesLabel);
-        gameStatsPanel.add(matchesLabel);
-        add(BorderLayout.NORTH, gameStatsPanel);
-
-
-
-
-
-
-
-        //Initializes cards to be in pairs
-        cards = new Card[CARD_NAMES.length * 2];
-
-
-
-
-        for (int i = 0, j = 0; i < CARD_NAMES.length; i++)
-        {
-            cards[j++] = new Card("cardimages/" + CARD_NAMES[i]);
-            cards[j++] = new Card("cardimages/" + CARD_NAMES[i]);
-        }
-
-
-
-
-
-
-
-        //Initializes buttons
-        continueButton = new JButton("Continue");
-        exitButton = new JButton("Exit");
-
-
-
-
-        JPanel buttonsPanel = new JPanel(new FlowLayout());
-        buttonsPanel.add(continueButton);
-        buttonsPanel.add(exitButton);
-        add(BorderLayout.SOUTH, buttonsPanel);
-
-
-
-
-        continueButton.addActionListener(this);
-        exitButton.addActionListener(this);
-
-
-
-
-
-
-
-        // starts a new game
-        initializeNewGame();
-
-
-
-
+        // Start game
+        startGame();
     }
 
+    public void startGame() {
+        // Load card images
+        for (int i = 0; i < numCards / 2; i++) {
+            this.icons[i] = new ImageIcon("cards_front/front.png"); //change from "front.png" to -- "cards_front/front.png"
 
 
-
-    private void initializeNewGame()
-    //Shuffles cards and puts them into the cards panel
-    {
-
-
-
-
-        if (cardsPanel != null)
-        //Remove cards in user interface
-        {
-            remove(cardsPanel);
+            this.icons[i + numCards / 2] = new ImageIcon("cards_back/back " + i + ".png"); //changed from "back " + i + ".png" to -- "cards_back/back " + i + ".png"
         }
 
-
-
-
-        continueButton.setEnabled(false);
-
-
-
-
-        numMoves = 0;
-        numMatches = 0;
-
-
-
-
-        movesLabel.setText("Attempts: 0");
-        matchesLabel.setText("Matches: 0");
-
-
-
-
-        for (int i = 0; i < cards.length; i++)
-            //Shuffle the cards, s variable for "shuffle"
-        {
-            int s = i + (int) (Math.random() * (cards.length - i));
-            Card temp = cards[s];
-            cards[s] = cards[i];
-            cards[i] = temp;
+        // Shuffle cards
+        Random rand = new Random();
+        for (int i = 0; i < numCards; i++) {
+            int j = rand.nextInt(numCards);
+            int temp = this.cardNumbers[i];
+            this.cardNumbers[i] = this.cardNumbers[j];
+            this.cardNumbers[j] = temp;
         }
 
-
-
-
-        cardsPanel = new JPanel(new GridLayout(cards.length / 4, cards.length / 4));
-
-
-
-
-        for (Card card : cards)
-        {
-            card.addActionListener(this);
-            card.hideCard();
-            cardsPanel.add(card);
-        }
-
-
-
-
-        add(BorderLayout.CENTER, cardsPanel);
-
-
-
-
-        firstOpenedCard = null;
-        secondOpenedCard = null;
-
-
-
-
-        this.repaint();
-        this.revalidate();
-    }
-
-
-
-
-    private void evaluateScore()        //Evaluates the score of the user
-    {
-        int score = 0;
-
-
-
-
-        if (numMoves > 0)
-        {
-            score = (int) (((double) numMatches / (double) numMoves) * 100);
-        }
-
-
-
-
-        String result = "In " + numMoves + " move(s), "+ " your score was " + score + ", ";
-
-
-        if (score == 100)
-        {
-            result += "Congratulations! YOU WON";
-        } else if (score > 97)
-        {
-            result += "AMAZING!";
-        } else if (score > 80)
-        {
-            result += "Excellent!";
-        } else if (score > 60)
-        {
-            result += "Great";
-        } else if (score > 30)
-        {
-            result += "Not too bad";
-        } else if (score > 10)
-        {
-            result += "Let's aim higher";
-        } else
-        {
-            result += "Better luck next time";
-        }
-
-
-
-
-        JOptionPane.showMessageDialog(this, result);
-    }
-
-
-
-
-
-
-
-
-    private void evaluateOpenedCards()          //Evaluate the flipped cards and keep track of matches
-    {
-        if (firstOpenedCard != null && secondOpenedCard != null)
-        {
-            numMoves++;
-            if (firstOpenedCard.equals(secondOpenedCard))           //Check for matches
-            {
-                numMatches++;
-
-
-
-
-                firstOpenedCard = null;
-                secondOpenedCard = null;
-            } else {
-                continueButton.setEnabled(true);            //Continue button only works for mismatched cards
-            }
-            movesLabel.setText("Moves: " + numMoves);      //Update the stats
-            matchesLabel.setText("Matches: " + numMatches);
-            if (numMatches >= CARD_NAMES.length)            //If all were matched then evaluate the score
-            {
-                evaluateScore();
-            }
-        }
-
-
-
-
-    }
-
-
-
-
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if (e.getSource() instanceof Card)
-        {
-            if ((firstOpenedCard != null) && (secondOpenedCard != null))
-            {
-                return;
-            }
-
-
-
-
-            if (secondOpenedCard == null)
-            {
-                secondOpenedCard = (Card) e.getSource();
-                secondOpenedCard.showCard();
-                secondOpenedCard.removeActionListener(this);
-            } else if (null == firstOpenedCard) {
-                firstOpenedCard = (Card) e.getSource();
-                firstOpenedCard.showCard();
-                firstOpenedCard.removeActionListener(this);
-            }
-
-
-
-
-            evaluateOpenedCards();
-        } else if (e.getSource() == continueButton)
-        {
-            firstOpenedCard.hideCard();
-            secondOpenedCard.hideCard();
-
-
-
-
-            firstOpenedCard.addActionListener(this);
-            secondOpenedCard.addActionListener(this);
-
-
-
-
-            firstOpenedCard = null;
-            secondOpenedCard = null;
-
-
-
-
-            continueButton.setEnabled(false);
-        } else if (e.getSource() == exitButton)
-            //Asks for confirmation if you want to close the gaming program
-        {
-            if (JOptionPane.showConfirmDialog(null, "Are you sure you want to quit the game?") != JOptionPane.YES_OPTION)
-            {
-                return;
-            }
-
-
-
-            // Evaluates card matches
-            if (numMatches < CARD_NAMES.length)
-            {
-                evaluateScore();
-            }
-
-
-
-
-            setVisible(false);
-            dispose();
+        // Reset game variables
+        Arrays.fill(this.matchedCards, 0);
+        Arrays.fill(this.selectedCards, -1);
+        this.numMatches = 0;
+        this.numAttempts = 0;
+        this.timeLeft = 300;
+        this.timer.start();
+
+        // Update buttons
+        for (int i = 0; i < numCards; i++) {
+            this.buttons[i].setIcon(this.icons[this.cardNumbers[i]]);
+            this.buttons[i].setEnabled(true);
         }
     }
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JButton) {
+            // Handle button click
+            JButton button = (JButton) e.getSource();
+            int index = Arrays.asList(this.buttons).indexOf(button);
+            if (this.matchedCards[index] == 0 && this.selectedCards[0] != index) {
+                if (this.selectedCards[0] == -1) {
+                    this.selectedCards[0] = index;
+                    this.buttons[index].setIcon(this.icons[this.cardNumbers[index]]);
+                } else {
+                    this.selectedCards[1] = index;
+                    this.buttons[index].setIcon(this.icons[this.cardNumbers[index]]);
+                    this.numAttempts++;
+                    if (this.cardNumbers[this.selectedCards[0]] == this.cardNumbers[this.selectedCards[1]]) {
+                        // Match found
+                        this.matchedCards[this.selectedCards[0]] = 1;
+                        this.matchedCards[this.selectedCards[1]] = 1;
+                        this.numMatches++;
+                        if (this.numMatches == this.numCards / 2) {
+                            // Game over
+                            this.timer.stop();
+                            JOptionPane.showMessageDialog(this, "Congratulations! You won in " + (300 - this.timeLeft) + " seconds with " + this.numAttempts + " attempts.");
+                            startGame();
+                        }
+                    } else {
+                        // No match found
+                        this.buttons[this.selectedCards[0]].setEnabled(false);
+                        this.buttons[this.selectedCards[1]].setEnabled(false);
+                        Timer flipTimer = new Timer(5000, new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                buttons[selectedCards[0]].setIcon(icons[cardNumbers[selectedCards[0]] + numCards / 2]);
+                                buttons[selectedCards[1]].setIcon(icons[cardNumbers[selectedCards[1]] + numCards / 2]);
+                                buttons[selectedCards[0]].setEnabled(true);
+                                buttons[selectedCards[1]].setEnabled(true);
+                                selectedCards[0] = -1;
+                                selectedCards[1] = -1;
+                            }
+                        });
+                        flipTimer.setRepeats(false);
+                        flipTimer.start();
+                    }
+                }
+            }
+        } else if (e.getSource() == this.timer) {
+            // Handle timer tick
+            this.timeLeft--;
+            this.timerLabel.setText("Time left: " + this.timeLeft + " seconds");
+            if (this.timeLeft == 0) {
+                // Game over
+                this.timer.stop();
+                JOptionPane.showMessageDialog(this, "Time's up! You had " + this.numMatches + " matches and " + this.numAttempts + " attempts.");
+                startGame();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                // Prompts the user for an input value of 12 or 24 (cards) for specified grid size
-                String gridSizeInput = JOptionPane.showInputDialog("Grid size/number of cards: Enter '12' or '24':");
-                int gridSize = Integer.parseInt(gridSizeInput);
-
-
-
-
-
-
-
-
-                if (gridSize != 12 && gridSize != 24) {
-                    JOptionPane.showInputDialog("Invalid grid size. Only enter 12 or 24. ");
-                    return;
-                }
-
-
-                new FlipperGame().setVisible(true);
-            }
-        });
+        int numCards = Integer.parseInt(JOptionPane.showInputDialog("Enter number of cards (12 or 24): "));
+        if (numCards != 12 && numCards != 24) {
+            JOptionPane.showMessageDialog(null, "Invalid number of cards. Defaulting to 12.");
+            numCards = 12;
+        }
+        new FlipperGame(numCards);
     }
 }
-
